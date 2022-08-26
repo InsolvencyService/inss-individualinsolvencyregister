fec60cefe906e0d3545d9da497590a6fcb3110b9
Jamie Coales <jamie@bluepillconsulting.com>
2022-08-26 15:46:43 +0100

    update search model

--- INSS.EIIR.Models/SearchModels/IndividualSearchModel.cs
+++ INSS.EIIR.Models/SearchModels/IndividualSearchModel.cs
@@ -2,11 +2,30 @@
 
 public class IndividualSearchModel
 {
+
+    //searchable fields
+    public string CaseNumber { get; set; }
+
     public string FirstName { get; set; }
 
-    public string Surname { get; set; }
+    public string MiddleName { get; set; }
 
+    public string FamilyName { get; set; }
+
+    public string AlternativeNames { get; set; }
+
+    public string LastKnownPostcode { get; set; }
+
+    public string TradingName { get; set; }
+
+    public string TradingPostcode { get; set; }
+
+    //Filterable Fields
     public List<string> Courts { get; set; }
 
     public List<string> CourtNames { get; set; }
+
+    public List<string> InsolvencyTypes { get; set; }
+
+    public List<string> InsolvencyServiceOffices { get; set; }
 }
