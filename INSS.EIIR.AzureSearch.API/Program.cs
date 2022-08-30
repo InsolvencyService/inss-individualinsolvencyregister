c08b8b1509b92497ff226cfa73f9740c1b542227
Jamie Coales <jamie@bluepillconsulting.com>
2022-08-30 14:04:46 +0100

    configure api services and ioc

--- INSS.EIIR.AzureSearch.API/Program.cs
+++ INSS.EIIR.AzureSearch.API/Program.cs
@@ -1,11 +1,14 @@
+using AutoMapper;
+using INSS.EIIR.AzureSearch.AutoMapperProfiles;
+using INSS.EIIR.AzureSearch.Services;
+using INSS.EIIR.AzureSearch.Services.ODataFilters;
+using INSS.EIIR.AzureSearch.Services.QueryServices;
+using INSS.EIIR.Interfaces.AzureSearch;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+ConfigureServices(builder.Services);
 
 var app = builder.Build();
 
@@ -23,3 +26,31 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
+void ConfigureServices(IServiceCollection services)
+{
+    // Add services to the container.
+    services.AddTransient<IIndividualQueryService, IndividualQueryService>();
+    
+    services.AddTransient<IIndiviualSearchFilter, IndividualSearchCourtFilter>();
+    services.AddTransient<IIndiviualSearchFilter, IndividualSearchCourtNameFilter>();
+
+    services.AddTransient<ISearchTermFormattingService, SearchTermFormattingService>();
+    services.AddTransient<ISearchCleaningService, SearchCleaningService>();
+
+    services.AddControllers();
+
+    // Auto Mapper Configurations
+    var mapperConfig = new MapperConfiguration(mc =>
+    {
+        mc.AddProfile(new IndividualSearchMapper());
+    });
+
+    var mapper = mapperConfig.CreateMapper();
+    services.AddSingleton(mapper);
+
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    services.AddEndpointsApiExplorer();
+    services.AddSwaggerGen();
+}
