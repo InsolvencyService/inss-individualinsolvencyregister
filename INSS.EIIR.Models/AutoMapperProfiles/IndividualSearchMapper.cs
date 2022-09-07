02c27b43a9c1f81cd97c808d558fc6c5b904567a
Jamie Coales <jamie@bluepillconsulting.com>
2022-09-07 13:56:23 +0100

    fix test project after moving functions project

--- INSS.EIIR.Models/AutoMapperProfiles/IndividualSearchMapper.cs
+++ INSS.EIIR.Models/AutoMapperProfiles/IndividualSearchMapper.cs
@@ -9,6 +9,7 @@ public class IndividualSearchMapper : Profile
     {
         CreateMap<SearchResult, IndividualSearch>()
             .ForMember(m => m.CaseNumber, opt => opt.MapFrom(s => s.CaseNo))
+            .ForMember(m => m.IndividualNumber, opt => opt.MapFrom(s => s.IndivNo))
             .ForMember(m => m.Court, opt => opt.MapFrom(s => s.Court))
             .ForMember(m => m.FirstName, opt => opt.MapFrom(s => s.FirstName))
             .ForMember(m => m.FamilyName, opt => opt.MapFrom(s => s.Surname));
