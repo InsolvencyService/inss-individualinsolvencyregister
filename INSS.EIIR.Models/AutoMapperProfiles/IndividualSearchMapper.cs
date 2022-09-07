5c0112b137aa493a00d430728bc20a71f387b104
Jamie Coales <jamie@bluepillconsulting.com>
2022-09-07 12:39:45 +0100

    populate key index field in automapper profile

--- INSS.EIIR.Models/AutoMapperProfiles/IndividualSearchMapper.cs
+++ INSS.EIIR.Models/AutoMapperProfiles/IndividualSearchMapper.cs
@@ -1,14 +1,16 @@
 ﻿using AutoMapper;
-using INSS.EIIR.Models;
 using INSS.EIIR.Models.IndexModels;
 
-namespace INSS.EIIR.AzureSearch.AutoMapperProfiles;
+namespace INSS.EIIR.Models.AutoMapperProfiles;
 
 public class IndividualSearchMapper : Profile
 {
     public IndividualSearchMapper()
     {
         CreateMap<SearchResult, IndividualSearch>()
+            .ForMember(m => m.CaseNumber, opt => opt.MapFrom(s => s.CaseNo))
+            .ForMember(m => m.Court, opt => opt.MapFrom(s => s.Court))
+            .ForMember(m => m.FirstName, opt => opt.MapFrom(s => s.FirstName))
             .ForMember(m => m.FamilyName, opt => opt.MapFrom(s => s.Surname));
     }
 }
