5f95d323c4b24eccb932e5830e11dad6d4e47350
Jamie Coales <jamie@bluepillconsulting.com>
2022-09-07 09:55:47 +0100

    add middleware

--- INSS.EIIR.Web/Controllers/HomeController.cs
+++ INSS.EIIR.Web/Controllers/HomeController.cs
@@ -1,6 +1,4 @@
-﻿using System.Diagnostics;
-using INSS.EIIR.Web.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace INSS.EIIR.Web.Controllers
 {
@@ -21,12 +19,6 @@ namespace INSS.EIIR.Web.Controllers
         public IActionResult Privacy()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
