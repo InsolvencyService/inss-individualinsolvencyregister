44ce6798fe14f3dc9b5233c1a3c64c447041c038
Jamie Coales <jamie@bluepillconsulting.com>
2022-09-09 12:39:25 +0100

    rework status code handling and add 503 page

--- INSS.EIIR.Web/Program.cs
+++ INSS.EIIR.Web/Program.cs
@@ -8,26 +8,12 @@ ConfigureServices(builder.Services);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.Use(async (ctx, next) =>
-    {
-        await next();
+app.UseStatusCodePagesWithReExecute("/errors", "?statusCode={0}");
 
-        if (ctx.Response.StatusCode == 404 && !ctx.Response.HasStarted)
-        {
-            var originalPath = ctx.Request.Path.Value ?? "unknown";
-            ctx.Items["originalPath"] = originalPath;
-            ctx.Request.Path = "/Errors/NotFound";
-            await next();
-        }
-    });
+app.UseExceptionHandler("/Errors");
+// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+app.UseHsts();
 
-    app.UseExceptionHandler("/Errors");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
