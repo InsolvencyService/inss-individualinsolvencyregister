104313aa52aba44a2f7f21ce90b55e0aae4871d4
Jamie Coales <jamie@bluepillconsulting.com>
2022-09-16 15:56:23 +0100

    fix asset locations

--- INSS.EIIR.Web/webpack.config.js
+++ INSS.EIIR.Web/webpack.config.js
@@ -92,14 +92,14 @@ module.exports = {
                 test: /\.(png|jpe?g|gif)$/,
                 type: 'asset/resource',
                 generator: {
-                    filename: './images/[name][ext]',
+                    filename: './assets/images/[name][ext]',
                 }
             },
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/,
                 type: 'asset/resource',
                 generator: {
-                    filename: './fonts/[name][ext]',
+                    filename: './assets/fonts/[name][ext]',
                 }
             }
         ]
