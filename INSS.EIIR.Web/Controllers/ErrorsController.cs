5b081e82cfd22d3de4ad15444abcbbb6512bc5d1
Shweta Yerawar <Shweta.Yerawar@bbr.com>
2022-09-29 15:43:54 +0100

    changed the data type and adding the one of the code which testers are facing

--- INSS.EIIR.Web/Controllers/ErrorsController.cs
+++ INSS.EIIR.Web/Controllers/ErrorsController.cs
@@ -5,7 +5,7 @@ namespace INSS.EIIR.Web.Controllers
     public class ErrorsController : Controller
     {
         [Route("/errors/{statusCode?}")]
-        public IActionResult Index(int? statusCode)
+        public IActionResult Index(double? statusCode)
         {
             if (statusCode != null)
             {
@@ -16,6 +16,8 @@ namespace INSS.EIIR.Web.Controllers
                     case 503:
                         return View("Maintenance");
                     case 500.30:
+                        return View("Index");
+                    case 500:
                         return View("Index");
                 }
             }
