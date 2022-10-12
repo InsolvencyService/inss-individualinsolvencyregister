f2da7b1182f1505c5720c466b0fad1e0914f8fd5
NomensaMayur <mrana@nomensa.com>
2022-10-12 13:13:48 +0100

    initial automation

---
+++ INSS.EIIR.QA.Automation/INSS.EIIR.QA.Automation/INSS.EIIR.QA.Automation/Pages/EIIRHomePage.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using INSS.EIIR.QA.Automation.Helpers;
+using System;
+using System.Threading;
+
+namespace INSS.EIIR.QA.Automation.Pages
+{
+    internal class EIIRHomePage : ElementHelper
+    {
+        private static string pageUrl { get; } = "https://app-uksouth-sit-eiir-web.azurewebsites.net/";
+        private static string pageTitle { get; } = "Home Page - INSS.EIIR.Web";
+        private static string pageHeader { get; } = "Default page template";
+
+        public static void verifyEIIRHomePage()
+        {
+            Assert.IsTrue(WebDriver.Url.Contains(pageUrl));
+            Assert.AreEqual(pageTitle, WebDriver.Title);
+        }
+
+    }
+}
---
+++ INSS.EIIR.QA.Automation/INSS.EIIR.QA.Automation/INSS.EIIR.QA.Automation/TestFramework/TestSupport/Base.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+
+namespace INSS.EIIR.QA.Automation.TestFramework.TestSupport
+{
+    [Binding]
+    public class Base
+    {
+        public static IWebDriver WebDriver { get; private set; }
+
+        [Before]
+        public static void SetUp()
+        {
+            var browser = WebDriverFactory.Config["Browser"];
+
+            switch (browser)
+            {
+                case "Chrome":
+                    var chromeOptions = new ChromeOptions();
+                    //chromeOptions.AddArguments("--incognito", "--headless");
+                    //chromeOptions.AddArguments("--incognito");
+                    chromeOptions.AddUserProfilePreference("download.default_directory", FileHelper.GetDownloadFolder());
+                    WebDriver = new ChromeDriver(chromeOptions);
+                    break;
+
+                default:
+                    throw new Exception($"Driver name - {browser} does not match OR this framework does not support the webDriver specified");
+            }
+        }
+
+        [After]
+        public void CleanUp()
+        {
+            WebDriver.Quit();
+        }
+
+    }
+}
