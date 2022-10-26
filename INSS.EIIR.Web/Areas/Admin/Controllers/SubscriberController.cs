9b3e517b567df7fa2738dd2bfd83e06246c09ce7
spartacus72 <jeff@cumpsty.co.uk>
2022-10-26 09:54:15 +0100

    atu-84

--- INSS.EIIR.Web/Areas/Admin/Controllers/SubscriberController.cs
+++ INSS.EIIR.Web/Areas/Admin/Controllers/SubscriberController.cs
@@ -1,17 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Net.Mail;
 using System.Threading.Tasks;
 using INSS.EIIR.Interfaces.Services;
 using INSS.EIIR.Models.Constants;
+using INSS.EIIR.Models.SubscriberModels;
 using INSS.EIIR.Web.Constants;
+using INSS.EIIR.Web.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.CodeAnalysis.Differencing;
+using Microsoft.VisualBasic;
+using static System.Net.Mime.MediaTypeNames;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace INSS.EIIR.Web.Areas.Admin.Controllers
 {
+
     public class SubscriberController : Controller
     {
         private readonly ISubscriberDataProvider _subscriberDataProvider;
@@ -38,11 +47,74 @@ namespace INSS.EIIR.Web.Areas.Admin.Controllers
         }
 
         [Area(AreaNames.Admin)]
-        [Route(AreaNames.Admin + "/subscriber/{subscriberId}/change-profile")]
+        [HttpGet(AreaNames.Admin + "/subscriber/{subscriberId}/change-profile")]
         [Authorize(Roles = Role.Admin)]
         public async Task<IActionResult> ChangeProfile(int subscriberId)
         {
             var subscriber = await _subscriberDataProvider.GetSubscriberByIdAsync($"{subscriberId}");
+
+            var subscriberProfile = new SubscriberProfile
+            {
+                SubscriberId = subscriberId,
+                OrganisationName = subscriber.OrganisationName,
+                OrganisationType = subscriber.SubscriberDetails.OrganisationType,
+                ContactTitle = subscriber.SubscriberDetails.ContactTitle,
+                ContactForename = subscriber.SubscriberDetails.ContactForename,
+                ContactSurname = subscriber.SubscriberDetails.ContactSurname,
+                ContactAddress1 = subscriber.SubscriberDetails.ContactAddress1,
+                ContactAddress2 = subscriber.SubscriberDetails.ContactAddress2,
+                ContactCity = subscriber.SubscriberDetails.ContactCity,
+                ContactPostcode = subscriber.SubscriberDetails.ContactPostcode,
+                ContactEmail = subscriber.SubscriberDetails.ContactEmail,
+                ContactTelephone = subscriber.SubscriberDetails.ContactTelephone,
+                ApplicationDay = subscriber.AuthorisedDate?.Day,
+                ApplicationMonth = subscriber.AuthorisedDate?.Month,
+                ApplicationYear = subscriber.AuthorisedDate?.Year,
+                SubscribedFromDay = subscriber.SubscribedFrom?.Day,
+                SubscribedFromMonth = subscriber.SubscribedFrom?.Month,
+                SubscribedFromYear = subscriber.SubscribedFrom?.Year,
+                SubscribedToDay = subscriber.SubscribedTo?.Day,
+                SubscribedToMonth = subscriber.SubscribedTo?.Month,
+                SubscribedToYear = subscriber.SubscribedTo?.Year,
+                EmailAddress1 = subscriber.EmailContacts.Select(e => e.EmailAddress).FirstOrDefault(),
+                EmailAddress2 = subscriber.EmailContacts.Skip(1).Select(e => e.EmailAddress).FirstOrDefault(),
+                EmailAddress3 = subscriber.EmailContacts.Skip(2).Select(e => e.EmailAddress).FirstOrDefault(),
+                AccountActive = subscriber.AccountActive,
+
+            };
+
+            return View(subscriberProfile);
+        }
+
+        [Area(AreaNames.Admin)]
+        [HttpPost(AreaNames.Admin + "/subscriber/change-profile")]
+        [Authorize(Roles = Role.Admin)]
+        public async Task<IActionResult> ChangeProfile(SubscriberProfile subscriber)
+        {
+            if (ModelState.IsValid)
+            {
+                var createUpdateSubscriber = new CreateUpdateSubscriber
+                {
+                    AccountActive = subscriber.AccountActive,
+                    ApplicationDate = subscriber.ApplicationDate,
+                    ContactAddress1 = subscriber.ContactAddress1,
+                    ContactAddress2 = subscriber.ContactAddress2,
+                    ContactCity = subscriber.ContactCity,
+                    ContactEmail = subscriber.ContactEmail,
+                    ContactForename = subscriber.ContactForename,
+                    ContactPostcode = subscriber.ContactPostcode,
+                    ContactSurname = subscriber.ContactSurname,
+                    ContactTelephone = subscriber.ContactTelephone,
+                    EmailAddresses = subscriber.EmailAddresses.ToList(),
+                    OrganisationName = subscriber.OrganisationName,
+                    OrganisationType = subscriber.OrganisationType,
+                    SubscribedFrom = subscriber.SubscribedFrom,
+                    SubscribedTo = subscriber.SubscribedTo
+                    
+                };
+
+                await _subscriberDataProvider.UpdateSubscriberAsync($"{subscriber.SubscriberId}", createUpdateSubscriber);
+            }
 
             return View(subscriber);
         }
