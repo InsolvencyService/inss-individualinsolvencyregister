954260f020ca234a16beede330249d7d4ae0d9db
Jeffrey Cumpsty <jeff@cumpsty.co.uk>
2022-10-28 16:53:05 +0100

    Atu 84 admin update subscriber details (#73)
    * atu-84
    
    * merge
    
    * 84
    
    * atu-84
    
    * atu84
    
    * atu-84
    
    * 84
    
    * service
    
    * services
    
    * .
    
    * 84
    
    * remove settings

---
+++ INSS.EIIR.Web.Services/SubscriberService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Flurl;
+using System.Reflection.Metadata;
+using INSS.EIIR.Interfaces.DataAccess;
+using INSS.EIIR.Interfaces.Web.Services;
+using INSS.EIIR.Models.Configuration;
+using INSS.EIIR.Models.SubscriberModels;
+using Microsoft.Extensions.Options;
+using Flurl.Http;
+
+namespace INSS.EIIR.Web.Services
+{
+    public class SubscriberService : ISubscriberService
+    {
+        private readonly ApiSettings _settings;
+
+        public SubscriberService(IOptions<ApiSettings> settings)
+        {
+            _settings = settings.Value;
+        }
+
+        public async Task<Subscriber> GetSubscriberByIdAsync(string subscriberId)
+        {
+            return await _settings.BaseUrl
+                .AppendPathSegment($"subscribers/{subscriberId}")
+                .WithHeader("x-functions-key", _settings.ApiKey)
+                .GetJsonAsync<Subscriber>();
+        }
+
+        public async Task UpdateSubscriberAsync(string subscriberId, CreateUpdateSubscriber subscriber)
+        {
+            await _settings.BaseUrl
+                .AppendPathSegment($"subscribers/{subscriberId}/update")
+                .WithHeader("x-functions-key", _settings.ApiKey)
+                .PutJsonAsync(subscriber);
+        }
+    }
+}
+
