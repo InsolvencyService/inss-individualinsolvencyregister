d8c7ef9d270af7fca78c94ef1b04cfee7caba2e2
Jamie Coales <jamie@bluepillconsulting.com>
2022-11-01 09:54:48 +0000

    empty search error page

--- INSS.EIIR.Models/Home/StaticContent.cs
+++ INSS.EIIR.Models/Home/StaticContent.cs
@@ -1,10 +1,5 @@
 ﻿using INSS.EIIR.Models.Breadcrumb;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace INSS.EIIR.Models.Home
 {
