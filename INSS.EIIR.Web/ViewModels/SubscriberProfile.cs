e72632dbff5cd140f7a92352d1212f49c33c8fa4
Jamie Coales <jamie@bluepillconsulting.com>
2022-12-19 14:09:04 +0000

    subscriber validation fix

--- INSS.EIIR.Web/ViewModels/SubscriberProfile.cs
+++ INSS.EIIR.Web/ViewModels/SubscriberProfile.cs
@@ -8,6 +8,9 @@ namespace INSS.EIIR.Web.ViewModels
 {
     public class SubscriberProfile
     {
+        private const string NameValidCharacters = "^[a-zA-Z ]+(([',.\\-][a-zA-Z ])?[a-zA-Z ]*)*$";
+        private const string AddressValidCharacters = "^[a-zA-Z0-9 ]+(([',.\\-][a-zA-Z0-9 ])?[a-zA-Z0-9 ]*)*$";
+        private const string InvalidCharactersValidationError = "{0} contains invalid characters";
 
         public SubscriberProfile()
         {
@@ -18,6 +21,7 @@ namespace INSS.EIIR.Web.ViewModels
         [Required(ErrorMessage = "Enter the name of the company or organisation")]
         [MaxLength(50)]
         [Display(Name = "Name")]
+        [RegularExpression(NameValidCharacters, ErrorMessage = InvalidCharactersValidationError)]
         public string OrganisationName { get; set; }
 
         [Required(ErrorMessage = "Select the type of company or organisation")]
@@ -28,25 +32,30 @@ namespace INSS.EIIR.Web.ViewModels
         [Required(ErrorMessage = "Enter the first name")]
         [Display(Name = "First name")]
         [MaxLength(40)]
+        [RegularExpression(NameValidCharacters, ErrorMessage = InvalidCharactersValidationError)]
         public string ContactForename { get; set; }
 
         [Required(ErrorMessage = "Enter the last name")]
         [Display(Name = "Last name")]
         [MaxLength(40)]
+        [RegularExpression(NameValidCharacters, ErrorMessage = InvalidCharactersValidationError)]
         public string ContactSurname { get; set; }
 
         [Required(ErrorMessage = "Enter line 1 of the address")]
         [Display(Name = "Address line 1")]
         [MaxLength(60)]
+        [RegularExpression(AddressValidCharacters, ErrorMessage = InvalidCharactersValidationError)]
         public string ContactAddress1 { get; set; }
 
         [Display(Name = "Address line 2")]
         [MaxLength(60)]
+        [RegularExpression(AddressValidCharacters, ErrorMessage = InvalidCharactersValidationError)]
         public string ContactAddress2 { get; set; }
 
         [Required(ErrorMessage = "Enter the town or city")]
         [Display(Name = "Town or city")]
         [MaxLength(60)]
+        [RegularExpression(AddressValidCharacters, ErrorMessage = InvalidCharactersValidationError)]
         public string ContactCity { get; set; }
 
         [Required(ErrorMessage = "Enter the postcode")]
