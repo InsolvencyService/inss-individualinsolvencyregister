d70e1daab066032cc6fa9c44103262bb05e4769d
PRABHAT SHUKLA <Prabhat.Shukla@insolvency.gov.uk>
2024-01-26 10:39:04 +0000

    Added space

--- INSS.EIIR.AzureSearch.Tests/SearchIndexServiceTests.cs
+++ INSS.EIIR.AzureSearch.Tests/SearchIndexServiceTests.cs
@@ -52,6 +52,7 @@ namespace INSS.EIIR.AzureSearch.Tests
 
             Assert.Equal(caseData.FullName, string.Join(" ", caseData.FirstName, caseData.MiddleName, caseData.FamilyName));
             Assert.Equal(caseData.CombinedName, string.Join(" ", caseData.FirstName, caseData.FamilyName));
+
             dataProviderMock.Verify(m => m.GetIndividualSearchData(), Times.Once);
             mapperMock.Verify(m => m.Map<IEnumerable<CaseResult>, IEnumerable<IndividualSearch>>(rawData), Times.Once);
             indexClientMock.Verify(m => m.GetSearchClient("eiir_individuals"), Times.Once);
