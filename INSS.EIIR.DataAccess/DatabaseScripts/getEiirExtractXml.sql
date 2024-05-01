2acaae844eca5c6656d92fd149f10da4c98ab93d
John Macdonald <John.Macdonald@insolvency.gov.uk>
2024-05-01 09:07:06 +0100

    Revert "4944/11 Addition of SpecialNote to CaseDetails where there is a DeceasedDate"
    This reverts commit 002a4cf725d39a976a69bdf663df6c4e47976f03.

--- INSS.EIIR.DataAccess/DatabaseScripts/getEiirExtractXml.sql
+++ INSS.EIIR.DataAccess/DatabaseScripts/getEiirExtractXml.sql
@@ -979,7 +979,6 @@ SET @resultXML = (SELECT
 					END As CourtNumber,
 					caseYear AS CaseYear,
 					insolvencyDate AS StartDate,
-					'Please note that this person is deceased (Deceased Date ' + CONVERT(VARCHAR(10), deceasedDate, 103) + ')' AS SpecialNote,
 					TRIM(caseStatus) AS Status,
 					caseDescription AS CaseDescription, 
 					tradingNames AS TradingNames
--- INSS.EIIR.Functions/Functions/ExtractJobServiceTrigger.cs
+++ INSS.EIIR.Functions/Functions/ExtractJobServiceTrigger.cs
@@ -1,16 +1,9 @@
 using INSS.EIIR.Interfaces.DataAccess;
 using INSS.EIIR.Interfaces.Services;
 using INSS.EIIR.Models.ExtractModels;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
-using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
 using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 using System;
-using System.Net;
 using System.Threading.Tasks;
 
 namespace INSS.EIIR.Functions.Functions;
@@ -32,22 +25,7 @@ public class ExtractJobServiceTrigger
     }
 
     [FunctionName("ExtractJobServiceTrigger")]
-    [OpenApiOperation(operationId: "Run", tags: new[] { "Extract" })]
-    [OpenApiSecurity("apikeyheader_auth", SecuritySchemeType.ApiKey, In = OpenApiSecurityLocationType.Header, Name = "x-functions-key")]
-    [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
-    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req)
-    {
-        private_run(new ExtractJobMessage() { ExtractFilename = "Glen_Test_OrderBy_IPAddress", ExtractId = 123 });
-        string responseMessage = $"This eiir subscriber file creation has been triggered at: {DateTime.Now}";
-        return new OkObjectResult(responseMessage);
-    }
-
-    public async Task Runs([ServiceBusTrigger("%servicebusextractjobqueue%", Connection = "servicebussubscriberconnectionstring")] ExtractJobMessage message)
-    {
-        private_run(message);
-    }
-
-    private async Task private_run(ExtractJobMessage message)
+    public async Task Run([ServiceBusTrigger("%servicebusextractjobqueue%", Connection = "servicebussubscriberconnectionstring")] ExtractJobMessage message)
     {
         var now = DateTime.Now;
 
@@ -55,10 +33,10 @@ public class ExtractJobServiceTrigger
 
         try
         {
-            _extractService.GenerateSubscriberFile(message.ExtractFilename);
+            await _extractService.GenerateSubscriberFile(message.ExtractFilename);
 
             _eiirRepository.UpdateExtractAvailable();
-
+            
             _logger.LogInformation($"ExtractJobServiceTrigger ran succssfully on: {now} xml/zip file created with name: {message.ExtractFilename}");
         }
         catch (Exception ex)
@@ -68,28 +46,4 @@ public class ExtractJobServiceTrigger
             throw new Exception(error);
         }
     }
-
-
-    //[FunctionName("ExtractJobServiceTrigger")]
-    //public async Task Run([ServiceBusTrigger("%servicebusextractjobqueue%", Connection = "servicebussubscriberconnectionstring")] ExtractJobMessage message)
-    //{
-    //    var now = DateTime.Now;
-
-    //    _logger.LogInformation($"ExtractJobServiceTrigger received message: {message} on {now}");
-
-    //    try
-    //    {
-    //        await _extractService.GenerateSubscriberFile(message.ExtractFilename);
-
-    //        _eiirRepository.UpdateExtractAvailable();
-            
-    //        _logger.LogInformation($"ExtractJobServiceTrigger ran succssfully on: {now} xml/zip file created with name: {message.ExtractFilename}");
-    //    }
-    //    catch (Exception ex)
-    //    {
-    //        var error = $"ExtractJobServiceTrigger failed on: {now} with : {ex}";
-    //        _logger.LogError(error);
-    //        throw new Exception(error);
-    //    }
-    //}
 }
