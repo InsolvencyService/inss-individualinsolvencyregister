3f9860ce85a471be29fec22a31d249aa42308c17
Carl McIntyre <carl.mcintyre@insolvency.gov.uk>
2024-09-26 15:13:56 +0100

    added fake data source

---
+++ INSS.EIIR.StubbedTestData/Source/EiirFakeDataSource.cs
@@ -0,0 +1,33 @@
+﻿using INSS.EIIR.DataSync.Application.UseCase.SyncData.Infrastructure;
+using INSS.EIIR.DataSync.Application.UseCase.SyncData.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace INSS.EIIR.DataSync.Infrastructure.Fake.Source
+{
+    public class EiirFakeDataSource : IDataSourceAsync<InsolventIndividualRegisterModel>
+    {
+        List<InsolventIndividualRegisterModel> _data = new List<InsolventIndividualRegisterModel>();
+
+        public EiirFakeDataSource()
+        {
+            using (StreamReader r = new StreamReader("searchdata.json"))
+            {
+                string json = r.ReadToEnd();
+                _data = JsonSerializer.Deserialize<List<InsolventIndividualRegisterModel>>(json);
+            }
+        }
+
+        public async IAsyncEnumerable<InsolventIndividualRegisterModel> GetInsolventIndividualRegistrationsAsync()
+        {
+            foreach (var model in _data)
+            {
+                yield return model;
+            }
+        }
+    }
+}
