64e6813cbe0c42f8b2deb2d63de8fe4698e883a5
Carl McIntyre <carl.mcintyre@insolvency.gov.uk>
2024-09-30 14:25:26 +0100

    Ensuring that we keep eiir_individuals as an index for now..

--- INSS.EIIR.DataSync.Infrastructure/Sink/AISearch/IndexNameHelper.cs
+++ INSS.EIIR.DataSync.Infrastructure/Sink/AISearch/IndexNameHelper.cs
@@ -9,6 +9,8 @@ namespace INSS.EIIR.DataSync.Infrastructure.Sink.AISearch
 {
     public static class IndexNameHelper
     {
+        public const string EiirIndividuals = "eiir_individuals";
+
         public static async Task<string> GetNewIndexName(AsyncPageable<string> indexNames)
         {
             var todaysIndexName = $"{AISearchSink.SEARCH_INDEX_BASE_NAME}-{DateTime.Today.ToString("dd-MM-yyyy")}";
@@ -28,7 +30,8 @@ namespace INSS.EIIR.DataSync.Infrastructure.Sink.AISearch
         {
             var keepList = new List<string>
             {
-                keepMe
+                keepMe,
+                EiirIndividuals
             };
 
             var yesterday = DateTime.Today.AddDays(-1);
