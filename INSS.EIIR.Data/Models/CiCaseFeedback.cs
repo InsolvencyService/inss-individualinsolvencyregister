cf7a3342cc2213644aecdbef66654f0b89a6e394
John Macdonald <John.Macdonald@insolvency.gov.uk>
2024-12-18 17:25:05 +0000

    APP-5280 REemove references to tables ci_case and ci_indivdual when reviewing Feedback

--- INSS.EIIR.Data/Models/CiCaseFeedback.cs
+++ INSS.EIIR.Data/Models/CiCaseFeedback.cs
@@ -11,5 +11,9 @@
         public bool Viewed { get; set; }    
         public DateTime? ViewedDate { get; set; }
         public int CaseId { get; set; }
+        public string InsolvencyType { get; set; } = null!;
+        public string CaseName { get; set; } = null!;
+        public DateTime? InsolvencyDate { get; set; }
+        public int IndivNo { get; set; }
     }
 }
