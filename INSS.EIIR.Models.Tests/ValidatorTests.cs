6d1644eef4c54f92c9ef3d236597953fd87127ae
John Macdonald <John.Macdonald@insolvency.gov.uk>
2024-12-24 12:55:22 +0000

    APP-5306 Relax the validation on subscriber Organisation Name, contact first and last names

---
+++ INSS.EIIR.Models.Tests/ValidatorTests.cs
@@ -0,0 +1,42 @@
+﻿using INSS.EIIR.Models.CustomValidators;
+using System.ComponentModel.DataAnnotations;
+
+namespace INSS.EIIR.Models.Tests
+{
+    public class ValidatorTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("john was here")]
+        [InlineData("\".,<>)(*&^'")]
+        public void Only7bitCharacters_True(string value)
+        {
+            //Arrange
+            var attrib = new ContainsOnly7bitCharactersAttribute();
+
+            //Act
+            var validationResult = attrib.IsValid(value);
+
+            //Assert
+            Assert.True(validationResult);
+        }
+
+
+        [Theory]
+        [InlineData("’")]
+        [InlineData("This a test ’É")]
+        public void Only7bitCharacters_False(string value)
+        {
+            //Arrange
+            var attrib = new ContainsOnly7bitCharactersAttribute();
+            var context = new ValidationContext(value);
+
+            //Act
+            var validationResult = attrib.IsValid(value);
+
+            //Assert
+            Assert.False(validationResult);
+        }
+
+    }
+}
